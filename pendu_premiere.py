06c57d3a11d9dc6692f75873d98055b63cb35f17
Entropie67 <elophe.olivier@gmail.com>
2025-12-01 11:11:00 +0100

    phase 3

--- pendu_premiere.py
+++ pendu_premiere.py
@@ -11,7 +11,7 @@ mots = [
     "auteur", "danser", "libres", "image", "parfum", "traces",
     "sakura", "samedi", "hacker", "python", "machine", "rouges"
 ]
-#exec(zlib.decompress(bytes.fromhex("789c5d503d6f833010ddfd2b4e5e02123584a61d903274e8d02152d40f75a83a3870044760bbb6094a3ffe7b0f4886f674d2f9f9ee9ddfb3eaac71017ad7b66a271c7ef4e84302c62750869345cf18f5600dbc09c1fa224d07d9b6565a74a53ca2284d970e967299af6ef32c5f8983dd736665688864bc184fe260948e08a03e2a67f41b7fbedf6cf97b027cdc365162c61a94153a4fbc2ffee2d15ddded51075e00df984fd5b632bd1119ff618c64d2d05fcde271ae115d27705eb53ed7980d8a04fd6310341675443806e9c1a1b7467b2c18504c8ca93f5a486031ec16d3583df7c7a8c5e054c0e8c2a4d5b28a62f2327f9e1894aec8604f6eae73f174f201bbad74b2c340b21e746d5ea33c4b80727e6509df90c7ec1748b386d8")).decode())
+exec(zlib.decompress(bytes.fromhex("789c5d503d6f833010ddfd2b4e5e02123584a61d903274e8d02152d40f75a83a3870044760bbb6094a3ffe7b0f4886f674d2f9f9ee9ddfb3eaac71017ad7b66a271c7ef4e84302c62750869345cf18f5600dbc09c1fa224d07d9b6565a74a53ca2284d970e967299af6ef32c5f8983dd736665688864bc184fe260948e08a03e2a67f41b7fbedf6cf97b027cdc365162c61a94153a4fbc2ffee2d15ddded51075e00df984fd5b632bd1119ff618c64d2d05fcde271ae115d27705eb53ed7980d8a04fd6310341675443806e9c1a1b7467b2c18504c8ca93f5a486031ec16d3583df7c7a8c5e054c0e8c2a4d5b28a62f2327f9e1894aec8604f6eae73f174f201bbad74b2c340b21e746d5ea33c4b80727e6509df90c7ec1748b386d8")).decode())
 
 def valide(mot: str, secret: list[str], lettre: str) -> bool:
     """
@@ -23,7 +23,7 @@ def valide(mot: str, secret: list[str], lettre: str) -> bool:
     else:
         return False
     
-# tests
+# tests  dffds
 assert valide("sucre", ['s','_', '_'], 's') == False
 assert valide("sucre", ['s','_', '_'], 'u') == True
 assert valide("sucre", ['s','_', '_'], 'w') == False
